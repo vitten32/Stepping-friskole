68a61b2a1a7d5bcbd3c58989fef7a4a965384d79
vitten32 <vitten32@users.noreply.github.com>
2024-08-18 21:15:40 +0000

    deploy: 32562c5114ecca5620dbbfa9a0dd43eda6d491fc

---
+++ assets/BoenehusetPage-DFpuuasQ.js
@@ -0,0 +1,1 @@
+import{_ as o,x as a,K as t,ai as s,aj as c,ah as n}from"./index-CMsrGLp7.js";const d={name:"BoenehusetPage"},_=e=>(s("data-v-ce23a51d"),e=e(),c(),e),p=_(()=>n("h1",null,"Welcome to Boenehuset Page",-1)),r=[p];function h(e,u,i,l,f,m){return a(),t("div",null,r)}const x=o(d,[["render",h],["__scopeId","data-v-ce23a51d"]]);export{x as default};
